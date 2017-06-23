3dfd97ae06c050e422fd9709f9d4f2ae43061a44
Siddhartha Basu <biosidd@gmail.com>
2017-06-23 18:31:11 +0000

    Added check for empty input

--- src/Greenbox.js
+++ src/Greenbox.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Yellowbox from "./Yellowbox";
 
 
 class Greenbox extends React.Component {
 
 	// This syntax ensures `this` is bound within handleClick.
     // https://facebook.github.io/react/docs/handling-events.html
+    // It binds to the input element
 	handleChange = (event) => {
         // use camelCase not snake_case
-		this.setState( {newHobbie: event.target.value} );
+	    this.setState( {newHobbie: event.target.value} );
 	}
     // Read the link below to understand why we are using handlers
     // for every form components(such as input here).
 	// https://facebook.github.io/react/docs/forms.html#controlled-components
 	handleSubmit = (event) => {
-	    this.props.onNewHobbie(this.state.newHobbie);
+        // check for empty input
+        if (this.state.newHobbie) {
+	        this.props.onNewHobbie(this.state.newHobbie);
+        }
 	    event.preventDefault();
 	}
 
