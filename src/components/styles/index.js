8a34f986a817e8ea2bb8ebddf66629423d628dd3
Siddhartha Basu <biosidd@gmail.com>
2017-06-30 22:50:07 +0000

    Added Form and Input styled components

--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -14,6 +14,45 @@ const HeaderDiv = styled.div`
 	padding: 15px;
 	font-size: 2.5em;
 	color: white;
+
+    @media screen and (max-width: 600px){
+        font-size: 2.2em;
+    }
 `;
 
-export { Container, HeaderDiv }
+const GreenBox = styled.div`
+	height: 30px;
+    margin: 3% 1.5%;
+    padding: 5%;
+    background: #bedfc4;
+
+    @media screen and (max-width: 600px){
+    	height: 43px;
+    }
+`;
+
+const YellowBox = styled.div`
+    margin: 3% 1.5%;
+    padding: 3% 6%;
+    background: #fffab2;
+`;
+
+const Input = styled.input`
+	margin-right: 5px;
+`;
+
+const Submit = styled.input`
+`;
+
+const Form = styled.form`
+`;
+
+export {
+    Container,
+    HeaderDiv,
+    GreenBox,
+    YellowBox,
+    Submit,
+    Input,
+    Form
+}
