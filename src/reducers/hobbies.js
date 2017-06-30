6a25b07956865fc52ca9dc0950dbd4e2dca9424e
Siddhartha Basu <biosidd@gmail.com>
2017-06-30 14:09:29 +0000

    Changed the name to make more semantic sense

--- src/reducers/hobbies.js
+++ src/reducers/hobbies.js
@@ -4,15 +4,15 @@
 // 1. Reducer function is called by the "container" when there is a user action.
 // 2. If the reducer changes state, Redux passes the new state to each component
 //    and React re-renders each component.
-  
-const hobbieApp = (state = [], action) => {  //takes Redux state and returns a new array of hobbies (new state)
+
+const hobbieReducer = (state = [], action) => {  //takes Redux state and returns a new array of hobbies (new state)
   switch (action.type) {  //new Hobbie added if action's type is "ADD_HOBBIE"
     case 'ADD_HOBBIE':
       return [
         ...state,
         {
           id: action.id,
-          text: action.text       
+          text: action.text
         }
       ]
 
